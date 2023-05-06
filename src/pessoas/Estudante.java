1c57597870ed5b9bb5e8c13d4b9cc02d3384597b
Wal221 <walmir.cardoso@estudante.ifgoiano.edu.br>
2023-05-06 20:48:31 -0300

    Class Estudante ajeitada

--- src/pessoas/Estudante.java
+++ src/pessoas/Estudante.java
@@ -13,19 +13,21 @@ public class Estudante extends Usuario {
         this.matricula = matricula;
     }
 
+    public Estudante() {
+
+    }
 
 
-        @Override
-        public String toString () {
+    @Override
+    public String toString() {
 
-            StringBuilder estu = new StringBuilder();
-            estu.append("nome: " + getNome() + "\n");
-            estu.append("idade: " + getIdade() + "\n");
-            estu.append("matricula: " + matricula + "\n");
+        StringBuilder estu = new StringBuilder();
+        estu.append("nome: " + getNome() + "\n");
+        estu.append("idade: " + getIdade() + "\n");
+        estu.append("matricula: " + matricula + "\n");
 
-            return estu.toString();
-        }
-
+        return estu.toString();
+    }
 
 
     @Override
@@ -34,40 +36,39 @@ public class Estudante extends Usuario {
         String caminho;
         File arquivo = new File("C://Users//Administrador//biblioteca//src//arquivos" +
                 "estudante" + getNome() + ".txt");
+        try {
+            arquivo.createNewFile();
+            caminho = arquivo.getPath();
+            System.out.print("Arquivo criado com sucesso!");
 
-                    if( i > 3) {
+            FileWriter writer = new FileWriter(caminho, true);
+            writer.write("Nome: " + getNome() + "\n" + "Titulo: " + getBooks() + "\n" +
+                    "Emprestimo: " + getEmprestimos() + "\n");
+            writer.close();
 
-                        try {
-                            arquivo.createNewFile();
-                            caminho = arquivo.getPath();
-                            System.out.print("Arquivo criado com sucesso!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-                            FileWriter writer = new FileWriter(caminho, true);
-                            writer.write("Nome: " + getNome() + "\n" + "Titulo: " + getBooks() + "\n" +
-                                    "Emprestimo: " + getEmprestimos() + "\n");
-                            writer.close();
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }else{
-                        System.out.println("A quantidade de livros pemitida e somente 3");
-                    }
-                    i = limiteLivros();
-
-        //System.out.println("O limite de livros para estudantes e somente 3");
     }
 
+
+    /**
+     * @return Esse metodo vou usa primeiramente com duas intenções ,
+     * 1 de percorrer a lista da biblioteca e pegar o livro desejado pelo nome
+     * 2 com esse metodo posso contar a quantidade de linhas necessarias e vincula com a quantidade de livro que
+     * um usuario pode emprestimar
+     */
     @Override
-    public int limiteLivros() throws IOException {
-        LineNumberReader lnr = new LineNumberReader(new FileReader("C://Users//Administrador//POO2.0//src//Biblio//" +
-                "pessoas//estudante"+getNome()+".txt"));
+    public int numberLivros(String caminnho) throws IOException {
+        LineNumberReader lnr = new LineNumberReader(new FileReader(caminnho));
         lnr.skip(Long.MAX_VALUE);
         int retorno = lnr.getLineNumber();
 
         return retorno;
 
     }
+
     @Override
     public void excluir() {
 
@@ -83,15 +84,18 @@ public class Estudante extends Usuario {
 
     }
 
+    public void gravaEmprestismo() {
+        try {
+            FileWriter writer = new FileWriter("src/arquivos/Emprestimos", true);
+            writer.write("Nome: " + getNome() + "\n" + "Titulo do livro: " + getBooks() + "\n" + "Data: "
+                    + getEmprestimos());
+            writer.close();
+            System.out.println("livro cadastrado");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
-    public String livrosAssociados(){
-        StringBuilder estu = new StringBuilder();
-        estu.append("nome: " + getNome() + "\n");
-        estu.append("Dia do emprestimo:" + getEmprestimos().getDataDoEmprestimo());
-
-        return estu.toString();
     }
-
-    }
+}
 
